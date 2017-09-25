996db3651f35d6ea77221402622f33873c36c096
Edgar Chernick <edgarc@ciandt.com>
2017-09-25 15:03:14 -0300

    Fix controller

--- src/main/java/magiccards/controllers/CardsController.java
+++ src/main/java/magiccards/controllers/CardsController.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("magiccards")
+@RequestMapping("cards")
 public class CardsController {
 
 	@Autowired
